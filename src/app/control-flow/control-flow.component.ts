8cf70f24b03d08401d2ecc4d371134035fae31fb
nevzatopcu <nevzatopcu@gmail.com>
2023-11-13 19:31:46 +0300

    feat: added control flow examples

--- src/app/control-flow/control-flow.component.ts
+++ src/app/control-flow/control-flow.component.ts
@@ -1,7 +1,20 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, TrackByFunction } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { of } from 'rxjs';
 
 @Component({
   standalone: true,
   templateUrl: './control-flow.component.html',
+  imports: [MatCardModule, MatButtonModule, CommonModule],
 })
-export default class ControlFlowComponent {}
+export default class ControlFlowComponent {
+  isAdmin = true;
+  isAdmin$ = of(true);
+  userType: 'admin' | 'staff' | 'moderator' = 'admin';
+
+  fruits = ['apple', 'banana', 'mango', 'lime'];
+
+  fruitsTrackBy: TrackByFunction<string> = (_, fruit) => fruit;
+}
